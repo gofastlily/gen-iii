57b9490eddc7a91fbae76a28980dd4795978df65
AlexOn1ine <alexthenotes@gmail.com>
2024-07-12 14:53:09 +0200

    Pokabbie's variable battle speed ported by AlexOn1ine

--- src/palette.c
+++ src/palette.c
@@ -31,6 +31,7 @@ ALIGNED(4) EWRAM_DATA u16 gPlttBufferUnfaded[PLTT_BUFFER_SIZE] = {0};
 ALIGNED(4) EWRAM_DATA u16 gPlttBufferFaded[PLTT_BUFFER_SIZE] = {0};
 EWRAM_DATA struct PaletteFadeControl gPaletteFade = {0};
 static EWRAM_DATA u32 sPlttBufferTransferPending = 0;
+static EWRAM_DATA u32 sPlttPreviousUpdateResult = 0; // Fast Battle Speed
 
 static const u8 sRoundedDownGrayscaleMap[] = {
      0,  0,  0,  0,  0,
@@ -88,6 +89,8 @@ u32 UpdatePaletteFade(void)
 {
     u32 result;
 
+    sPlttPreviousUpdateResult = PALETTE_FADE_STATUS_LOADING;
+
     if (sPlttBufferTransferPending)
         return PALETTE_FADE_STATUS_LOADING;
 
@@ -101,8 +104,14 @@ u32 UpdatePaletteFade(void)
         result = UpdateHardwarePaletteFade();
 
     sPlttBufferTransferPending = gPaletteFade.multipurpose1;
+    sPlttPreviousUpdateResult = result;
 
     return result;
+}
+
+u32 PrevPaletteFadeResult(void)
+{
+    return sPlttPreviousUpdateResult;
 }
 
 void ResetPaletteFade(void)
