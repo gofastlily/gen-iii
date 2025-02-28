0df9f041b812ff7095fae0f727ffbde985ce1538
Lily <gofastlily@gmail.com>
2025-02-28 14:55:35 -0500

    Add altered time ratios similar to Majora's Mask

--- include/config/overworld.h
+++ include/config/overworld.h
@@ -100,11 +100,17 @@
 
 // These generational defines only make a distinction for OW_ALTERED_TIME_RATIO
 #define GEN_8_PLA                       GEN_LATEST + 2
+#define GEN_III_STANDARD                GEN_LATEST + 3
+#define GEN_III_SLOW                    GEN_LATEST + 4
 
 //Time
 #define OW_TIMES_OF_DAY                 GEN_LATEST // Different generations have the times of day change at different times.
-#define OW_USE_FAKE_RTC                 FALSE      // When TRUE, seconds on the in-game clock will only advance once every 60 playTimeVBlanks (every 60 frames).
-#define OW_ALTERED_TIME_RATIO           GEN_LATEST // In GEN_8_PLA, the time in game moves forward 60 seconds for every second in the RTC. In GEN_9, it is 20 seconds. This has no effect if OW_USE_FAKE_RTC is FALSE.
+#define OW_USE_FAKE_RTC                 TRUE      // When TRUE, seconds on the in-game clock will only advance once every 60 playTimeVBlanks (every 60 frames).
+#define OW_ALTERED_TIME_RATIO           GEN_III_STANDARD // This has no effect if OW_USE_FAKE_RTC is FALSE.
+                                                   // In GEN_8_PLA, the time in game moves forward 60 seconds for every second in the RTC.
+                                                   // In GEN_9, it is 20 seconds.
+                                                   // In GEN_III_STANDARD, it is 80 seconds.  This is based on the standard flow of time from Majora's Mask
+                                                   // In GEN_III_SLOW, it is 24 seconds.  This is based on the Inverted Song of Time from Majora's Mask
 
 // Overworld flags
 // To use the following features in scripting, replace the 0s with the flag ID you're assigning it to.
