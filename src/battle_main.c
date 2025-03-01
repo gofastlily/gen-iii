0254143ed68ad2acc372ab309920a151bce3d820
Lily <gofastlily@gmail.com>
2025-03-01 12:49:10 -0500

    Init clock to 10:00 and pause time when the game is paused

--- src/battle_main.c
+++ src/battle_main.c
@@ -418,6 +418,8 @@ const u8 *const gStatusConditionStringsTable[][2] =
 
 void CB2_InitBattle(void)
 {
+    FlagSet(FLAG_PAUSE_TIME);
+
     if (!gTestRunnerEnabled)
         MoveSaveBlocks_ResetHeap();
     AllocateBattleResources();
@@ -5713,6 +5715,8 @@ static void HandleEndTurn_FinishBattle(void)
         if (gBattleControllerExecFlags == 0)
             gBattleScriptingCommandsTable[gBattlescriptCurrInstr[0]]();
     }
+
+    FlagClear(FLAG_PAUSE_TIME);
 }
 
 static void FreeResetData_ReturnToOvOrDoEvolutions(void)
